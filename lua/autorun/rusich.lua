4d5318927558f49c0629264946aac86997e38702
Nomos2 <97733899+Nomos2@users.noreply.github.com>
2024-06-04 21:48:25 +0300

    Фикс
    фикс модели автосцепки.

--- lua/autorun/rusich.lua
+++ lua/autorun/rusich.lua
@@ -137,21 +137,6 @@ timer.Simple(0,function()
 				}
 				scripted_ents.Register(tbl,"gmod_train_bogey") -- регистрируем
 			end
-			
-    for k,v in pairs(Metrostroi.TrainClasses) do
-        if v == "gmod_subway_kuzov" then
-            Metrostroi.TrainClasses[k] = nil
-            Metrostroi.IsTrainClass[v] = nil
-            break
-		end	
-	if not Metrostroi.Advanced then return end	
-	for k,v in pairs(Metrostroi.Advanced.TrainClasses) do
-        if v == "gmod_subway_kuzov" then
-            Metrostroi.Advanced.TrainClasses[k] = nil
-            Metrostroi.Advanced.IsTrainClass[v] = nil
-		end	
-	end		
-	end			
 
 	-- регистрируем сцепку
 			local tbl = scripted_ents.Get("gmod_train_couple")
